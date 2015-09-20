233ee124006286538675f2bbb65a76b36fed147e
krll-k <ndsdmfwg@gmail.com>
2015-09-20 20:47:03 +0300

    add styles and render method

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var Game = function(){};
 Game.prototype.init = function(){
-	console.log(1);
+	// dt - data
+	this.dt = [];
+	for (var i = 0; i < 16; i++) {
+		this.dt[i] = 0;
+	};
+	return this;
+}
+Game.prototype.render = function(){
+	// prnt - parent
+	this.prnt = document.createElement('div');
+	// chld - child
+	this.chld = [];
+  for (var i = 0; i < this.dt.length; i++) {
+  	this.chld[i] = document.createElement('div');
+  	this.chld[i].textContent = this.dt[i];
+  	this.prnt.appendChild(this.chld[i]);
+  };
+	document.body.appendChild(this.prnt);
+	return this;
 }
 var game = new Game();
-game.init();
+game.init().render();
